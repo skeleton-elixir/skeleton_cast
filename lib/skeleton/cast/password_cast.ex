7f7d90e7b3754a3c79b4af9cb1fa0a447982da5f
Jhonathas Matos <jhonathas@gmail.com>
2020-07-17 18:37:04 -0300

    create cast of password

--- lib/skeleton/cast/password_cast.ex
+++ lib/skeleton/cast/password_cast.ex
@@ -1,13 +1,13 @@
 defmodule Skeleton.Cast.Password do
   import Ecto.Changeset
 
-  @password_regexp ~r/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
+  @password_regexp "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&,])(?=.{8,})"
 
   def cast_password(changeset, field, opts \\ []) do
     changeset
     |> cast(changeset.params, [field])
     |> validate_required_field(field, opts)
-    |> validate_password(field)
+    |> validate_password(field, opts)
   end
 
   defp validate_required_field(changeset, field, opts) do
@@ -18,11 +18,29 @@ defmodule Skeleton.Cast.Password do
     end
   end
 
-  defp validate_password(changeset, field) do
+  defp validate_password(changeset, field, opts) do
     if changeset.errors[field] do
       changeset
     else
-      validate_format(changeset, field, @password_regexp)
+      regexp = build_regexp(opts)
+
+      validate_format(changeset, field, regexp)
     end
+  end
+
+  defp build_regexp(opts), do: opts |> Enum.into(%{}) |> do_build_regexp()
+
+  defp do_build_regexp(%{regexp: r}) when is_binary(r), do: Regex.compile!(r)
+  defp do_build_regexp(%{regexp: r}), do: r
+  defp do_build_regexp(opts) do
+    regexp_list = []
+    regexp_list = regexp_list ++ if opts[:uppercase], do: ["(?=.*[A-Z])"], else: []
+    regexp_list = regexp_list ++ if opts[:downcase], do: ["(?=.*[a-z])"], else: []
+    regexp_list = regexp_list ++ if opts[:number], do: ["(?=.*[0-9])"], else: []
+    regexp_list = regexp_list ++ if opts[:character], do: ["(?=.*[@$!%*?&])"], else: []
+    regexp_list = regexp_list ++ if size = opts[:size], do: ["(?=.{#{size},})"], else: []
+    regexp_list = regexp_list ++ if regexp_list == [], do: [@password_regexp], else: regexp_list
+
+    regexp_list |> Enum.join("") |> Regex.compile!()
   end
 end
---
+++ test/skeleton/cast/password_cast_test.exs
@@ -0,0 +1,141 @@
+defmodule Skeleton.Cast.PasswordTest do
+  use Skeleton.Cast.TestCase
+  alias Skeleton.App.User
+  import Skeleton.Cast.Password
+
+  describe "Validating default regexp password" do
+    test "valid password" do
+      changeset = cast("Ab,11111")
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("1111")
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Validating password require downcase characters" do
+    test "valid password" do
+      changeset = cast("a11111111", downcase: true)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("11111111", downcase: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Validating password require uppercase characters" do
+    test "valid password" do
+      changeset = cast("A11111111", uppercase: true)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("A11111111", downcase: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Validating password require number" do
+    test "valid password" do
+      changeset = cast("abcdefgh1", number: true)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("abcdefghj", number: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Validating password size" do
+    test "valid password" do
+      changeset = cast("abc", size: 3)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("a", number: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Validating password with all options enabled" do
+    test "valid password" do
+      changeset = cast("abcdefghij1$L", size: 10, number: true, uppercase: true, downcase: true, character: true)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      changeset = cast("abcdefghij1cL", size: 10, number: true, uppercase: true, downcase: true, character: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  describe "Required password" do
+    test "valid password" do
+      changeset = cast("Abc,123456", required: true)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "requires when password is blank" do
+      changeset = cast("", required: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"can't be blank", [validation: :required]}]
+    end
+
+    test "requires when password is nil" do
+      changeset = cast(nil, required: true)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"can't be blank", [validation: :required]}]
+    end
+  end
+
+  describe "Validating custom regexp" do
+    test "valid password with string regexp" do
+      regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&,])(?=.{8,})"
+
+      changeset = cast("Ab,11111", regexp: regexp)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "valid password with sigil regexp" do
+      regexp = ~r/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&,])(?=.{8,})/
+
+      changeset = cast("Ab,11111", regexp: regexp)
+      assert changeset.valid?
+      assert changeset.changes[:password]
+    end
+
+    test "invalid password" do
+      regexp = ~r/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&,])(?=.{8,})/
+
+      changeset = cast("1111", regexp: regexp)
+      refute changeset.valid?
+      assert changeset.errors == [password: {"has invalid format", [validation: :format]}]
+    end
+  end
+
+  defp cast(password, opts \\ []) do
+    %User{}
+    |> cast(%{password: password}, [])
+    |> cast_password(:password, opts)
+  end
+end
